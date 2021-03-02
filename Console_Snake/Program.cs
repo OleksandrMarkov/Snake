49b88703e37555c97ed115b2ce7c684fb1362953
Oleksandr Markov <ol.markov1999@gmail.com>
2021-03-02 16:58:27 +0200

    borders

--- Console_Snake/Program.cs
+++ Console_Snake/Program.cs
@@ -6,12 +6,21 @@ namespace Console_Snake
 	class Program
 	{
 		public static void Main(string[] args)
-		{	
-		/*	Horizontal_line h_line = new Horizontal_line(2, 12, 4, '&');
-			h_line.display();
+		{
+			Console.SetWindowSize(80, 25);
+			Console.SetBufferSize(80, 25);
 			
-			Vertical_line v_line = new Vertical_line(7, 9, 13, '%');
-			v_line.display();*/
+			char border_sign = '*';
+			
+			Horizontal_line upper_border = new Horizontal_line(0, 78, 0, border_sign);
+			Horizontal_line bottom_border = new Horizontal_line(0, 78, 24, border_sign);
+			Vertical_line left_border = new Vertical_line(0, 0, 24, border_sign);
+			Vertical_line right_border = new Vertical_line(78, 0, 24, border_sign);
+			
+			upper_border.display();
+			bottom_border.display();
+			left_border.display();
+			right_border.display();
 			
 			Console.ReadKey(true);
 		}
